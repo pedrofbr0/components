36a8406e8056f981bc5f0ceb631ada51fe723b3f
pedrofbr0 <pedrofbr@hotmail.com>
2021-11-27 16:40:53 -0300

    Substitution of all hardcoded info in the component for props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -4,22 +4,22 @@
 // rest of the application know about it
 
 import React from 'react';
-import faker from 'faker';
+// import faker from 'faker';
 
 const CommentDetail = (props) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.avatar} />
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 5:42PM</span>
+                    <span className="date">{props.date}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.commentary}</div>
             </div>
         </div>
     );
